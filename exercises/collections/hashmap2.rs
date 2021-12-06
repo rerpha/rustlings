2fd641d0bf5c34e5e5ab59ad37731a78fdfb3079
Jack Harper <jack.harper@stfc.ac.uk>
2021-12-06 20:46:45 +0000

    commit

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -35,6 +35,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
+        basket.or_insert()
+        
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
